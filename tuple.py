25bb6984ccc204a509cade937a4259e2fc212cde
MOHAMMAD AMAN KHAN <146350867+ynotaman@users.noreply.github.com>
2023-10-03 23:53:29 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,13 @@
+list = ['manuu',786, 2.14 ,'john']
+print(list)
+print(" mutable property  :")
+print(list)
+print(" precedence :")
+list[3]='amaan'
+print(list[0])
+print(" slicing of a list : ")
+print(list[0:3]) 
+print (" make multiple copies  :")
+num=[10]*5
+print(num)
+
